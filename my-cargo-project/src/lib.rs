8e797aef4a48858f3aa1d443a651ebe30360f754
Roberto Casadei <robyonrails@gmail.com>
2019-05-23 16:57:21 +0200

    All kinds of tests

--- my-cargo-project/src/lib.rs
+++ my-cargo-project/src/lib.rs
@@ -1,6 +1,11 @@
 mod my_mod2;
 
 pub mod my_mod {
+    //! Documentation for the enclosing element
+    //!
+    //!     # use ::my_cargo_project::my_mod;
+    //!     assert!(my_mod::f() > 70);
+
     pub fn f() -> i32 {
         // return 77 + super::my_mod2::g(); // OK
         return 77 + crate::my_mod2::g(); // OK
@@ -9,6 +14,12 @@ pub mod my_mod {
         // return 77 + self::g(); // OK: self denotes path to current module
     }
 
+
+    /// Documentation comments are nice
+    ///
+    ///     # use ::my_cargo_project::my_mod::g;
+    ///     assert!(g()==0);
+    ///
     pub fn g() -> i32 { return 0; }
 
     #[test]
--- my-cargo-project/src/main.rs
+++ my-cargo-project/src/main.rs
@@ -5,3 +5,9 @@ use another_cargo_project::my_mod1::a;
 fn main() {
     println!("Hello, world! {} {}", my_mod::f(), a());
 }
+
+#[test]
+fn another_test(){
+    println!("Also the 'main', executable crate may have its own tests");
+    assert!(1 > 0);
+}
